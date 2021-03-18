23b33420a7bb4e87abf30f63d37104b0d0c8de9b
forsili <orsili.federico@gmail.com>
2021-03-18 22:25:38 +0100

    libft

---
+++ ft_print_matrix.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_matrix.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: forsili <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 22:21:34 by forsili           #+#    #+#             */
+/*   Updated: 2021/03/18 22:23:28 by forsili          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_print_matrix(char **matrix, char *prefix)
+{
+	int i;
+
+	i = 0;
+	if (!matrix)
+		return ;
+	while (matrix[i])
+	{
+		ft_printf("%s%s\n", prefix, matrix);
+		i++;
+	}
+}
